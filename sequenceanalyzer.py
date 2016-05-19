307216f64564999fb4ee1a71d77585106200c5a7
franchenstein <dfranch6@gmail.com>
2016-05-19 17:46:34 -0300

    Sequence analyzer's methods with header comment

--- sequenceanalyzer.py
+++ sequenceanalyzer.py
@@ -64,7 +64,7 @@ class SequenceAnalyzer():
     Input:
         *L: maximum sub-sequence length to be analyzed.
     Output: 
-        *conditional)probabilities: a list of dictionaries. Each dictionary 
+        *conditional_probabilities: a list of dictionaries. Each dictionary 
          contains keys that are of the form:
          symbol|subsequence
          meaning the probability of "symbol" occuring after that subsequence.
@@ -98,10 +98,21 @@ class SequenceAnalyzer():
             print "Run calc_probs function before this one." 
         return self.conditional_probabilities
         
+    '''
+    Name: calc_cond_entropy
+    Input:
+        *L: maximum sub-sequence length to be analyzed.
+    Output: 
+        *conditional_entropy: An array of conditional entropies for subsequences
+         of length up to L.
+    Description:
+        Calculates the conditional entropy of each symbol occuring given sub-
+        sequences of lengths up to L.
+    '''    
     def calc_cond_entropy(self, L):
         self.cond_entropy = [] 
-        try: 
-            try:   
+        if self.probabilities: 
+            if self.conditional_probabilities:   
                 l = 0
                 while l <= L:
                     acc = 0
@@ -116,29 +127,52 @@ class SequenceAnalyzer():
                                 acc -= p[x]*num.log2(pcond[y_given_x])
                     self.cond_entropy.append(acc)
                     l += 1
-            except:
+            else:
                 print "Conditional probabilities not computed."
                 print "Run calc_cond_probs function before this one." 
-        except:
+        else:
             print "Probabilities not computed."
             print "Run calc_probs function before this one."                 
         return self.cond_entropy        
-        
+    
+    '''
+    Name: calc_kldivergence
+    Input:
+        *base_probs: A list of probability dictionaries to which the 
+         probabilities contained in this class will be compared.
+        *K: The length/level of probabilities from each that will be compared.
+    Output: 
+        *kldivergence: The Kullback-Leibler Divergence between the probability
+         distributions of sequences of length K from base_probs and
+         self.probabilities.
+    Description:
+        Calculates the KL Divergence of prob distributions of K-length seqs.
+    '''       
     def calc_kldivergence(self, base_probs, K):
         self.kldivergence = 0
-        try:
+        if self.probabilities:
             for key in base_probs[K].keys():
                 p = base_probs[K][key]
                 if key in self.probabilities[K].keys():
                     q = self.probabilities[K][key]
                 else:
-                    q = 1e-15
+                    q = 1e-15 #Default non-zero really small value
                 self.kldivergence += p*num.log2(p/q)
-        except:
+        else:
             print "[error] Probabilities not computed."
             print "Run calc_probs function before this one." 
         return self.kldivergence              
-        
+     
+    '''
+    Name: calc_autocorrelation
+    Input:
+        *up_to: The maximum window to be used in the autocorrelation.
+    Output: 
+        *autocorrelation: the sequence's autocorrelation.
+    Description:
+        Calculates the correlation of the sequence with itself with a window 
+        size up to the input variable.
+    '''   
     def calc_autocorrelation(self, up_to):
         self.autocorrelation = []
         temp = []
@@ -150,11 +184,24 @@ class SequenceAnalyzer():
         m = max(temp)
         self.autocorrelation = [float(x)/(2*float(m)) for x in temp]
         return self.autocorrelation
-        
+    
+    '''
+    Name: calc_l1metric
+    Input:
+    	*base_probs: A list of probability dictionaries to which the 
+         probabilities contained in this class will be compared.
+        *up_to: The maximum window to be used in the metric calculation.
+    Output: 
+        *l1metric: the L1 metric to estimate divergence between sequences.
+    Description:
+        Calculates the L1 metric of the probability distributions of 
+        subsequences of lengths up to the input parameter between the base
+        sequence and the object's sequence.
+    '''      
     def calc_l1metric(self, base_probs, up_to):
         self.l1metric = 0
         acc = 0
-        try:
+        if self.probabilities:
             for i in range(0, up_to):
                 for key in base_probs[i].keys():
                     if key in self.probabilities[i].keys():
@@ -163,11 +210,23 @@ class SequenceAnalyzer():
                         v2 = 0
                     acc += abs(base_probs[i][key] - v2)/(2**i)
             self.l1metric = acc
-        except:
+        else:
             print "Probabilities not computed."
             print "Run calc_probs function before this one." 
         return self.l1metric
-        
+    
+    '''
+    Name: save_cond_probs_as_graph
+    Input:
+    	*path: path where the output graph will be saved.
+    Output:
+    Description:
+        Saves the conditional probabilities in the format described in graph.py
+        The conditional probabilities are used to form a Rooted Tree With Proba-
+        bilities where a given subsequence will be a state and its transitions
+        will be the conditional probabilities of each symbol of the alphabet
+        given the current state.
+    '''    
     def save_cond_probs_as_graph(self, path):
         if self.conditional_probabilities:
             i = 0
