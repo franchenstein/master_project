286b91a516af64ac91e7bd7d58d523d303859b61
franchenstein <dfranch6@gmail.com>
2016-09-18 01:23:06 -0300

    removed unnecessary triple structure from synchwordfinder gamma tree

--- synchwordfinder.py
+++ synchwordfinder.py
@@ -15,11 +15,9 @@ class SynchWordFinder:
         self.t = copy.deepcopy(self.s)
         for state in self.t.states:
             state.name = state.name[::-1]
-            self.psi[state.name] = []
             self.candidacy_flags[state.name] = True
             self.tested_flags[state.name] = False
-        e = self.s.root()
-        self.gamma = [[e, self.candidacy_flags[e.name], self.tested_flags[e.name]]]
+        self.gamma = [self.s.root()]
         self.delta = [self.t.root()] + self.t.root().obtain_children()
         self.omega_syn = []
         self.valid_suffixes = {}
@@ -28,7 +26,7 @@ class SynchWordFinder:
 
     def next_valid_state(self):
         candidates = [x for x in self.gamma
-                      if self.candidacy_flags[x[0].name] is True and self.tested_flags[x[0].name] is False]
+                      if self.candidacy_flags[x.name] is True and self.tested_flags[x.name] is False]
         if candidates:
             return candidates[0]
         else:
@@ -38,9 +36,9 @@ class SynchWordFinder:
         while True:
             c = self.next_valid_state()
             if c:
-                l = c[0].name_length()
+                l = c.name_length()
                 if l < self.w:
-                    candidate = c[0].name
+                    candidate = c.name
                     lamda = self.suffixes[candidate] if candidate in self.suffixes.keys() else []
                     if not lamda:
                             self.tested_flags[candidate] = True
@@ -49,42 +47,30 @@ class SynchWordFinder:
                         final = len(lamda)
                         for el in lamda:
                             for l2 in self.l2range:
-                                p = self.s.compare_morphs(c[0].extended_morph(l2), el.extended_morph(l2),
+                                p = self.s.compare_morphs(c.extended_morph(l2), el.extended_morph(l2),
                                                           self.alpha, self.test)
                                 if p[0] == False:
                                     break
                             self.suffixes[candidate].remove(el)
                             if p[0]:
                                 if count >= final:
-                                    self.tested_flags[c[0].name] = True
+                                    self.tested_flags[c.name] = True
                             else:
-                                self.candidacy_flags[c[0].name] = False
-                                self.expand_trees(c[0])
+                                self.candidacy_flags[c.name] = False
+                                self.expand_trees(c)
                                 self.gamma.remove(c)
                                 for g in self.gamma:
-                                    self.tested_flags[g[0].name] = False
+                                    self.tested_flags[g.name] = False
                                 break
                             count += 1
                             if count == final:
-                                self.tested_flags[c[0].name] = True
+                                self.tested_flags[c.name] = True
                 else:
                     self.gamma.remove(c)
             else:
-                self.omega_syn = [x[0].name for x in self.gamma if self.candidacy_flags[x[0].name] is True and
-                                  self.tested_flags[x[0].name] is True]
+                self.omega_syn = [x.name for x in self.gamma if self.candidacy_flags[x.name] is True and
+                                  self.tested_flags[x.name] is True]
                 return self.omega_syn
-
-    def is_suffix(self, candidate, string, state):
-        if candidate == 'e':
-            return True
-        elif not state:
-            return False
-        elif string == '':
-            return state.name == candidate
-        else:
-            nxt = state.next_state_from_edge(string[0])
-            rest = string[1:]
-            return self.is_suffix(candidate, rest, nxt)
 
     def shortest_valid_suffix(self, state, name):
         if not state:
@@ -106,7 +92,7 @@ class SynchWordFinder:
             short = self.shortest_valid_suffix(self.t.root(), gcs.name[::-1])
             if short:
                 if short.name == gcs.name:
-                    self.gamma.append([gcs, self.candidacy_flags[gcs.name], self.tested_flags[gcs.name]])
+                    self.gamma.append(gcs)
                     rev.append(gcs.name[::-1])
                 else:
                     if short.name in self.valid_suffixes.keys():
